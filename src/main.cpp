141a0e4d774456b96fb141b0c2663f20cb061f97
Ivan Kravchenko <iv.kravchenko@gmail.com>
2026-01-01 19:19:39 +0300

    Добавил вывод отметки времени.

--- src/main.cpp
+++ src/main.cpp
@@ -14,17 +14,15 @@
 esp_adc_cal_characteristics_t adc_chars;
 float silence_level_adc = 0;
 bool led_flag = false;
-OneButton onboard_btn(BTN0, true, true);
 
-constexpr uint16_t config_portal_timeout = 3000;
-constexpr uint16_t btn0_long_press_timeout = 3000;
+OneButton onboard_btn(BTN0, true, true);
 
 Preferences preferences;
 WiFiManager wm;
 
 
 uint32_t long_press_start = 0;
-bool during_long_press = false;
+bool time_error = true;
 
 volatile MIC_SIGNAL mic_signal;
 
@@ -34,7 +32,6 @@ void btn0_click() {
 }
 
 void btn0_pressed() {
-  during_long_press = true;
   long_press_start = millis();
   digitalWrite(LED_BUILTIN, LOW);
 
@@ -42,27 +39,24 @@ void btn0_pressed() {
 }
 
 void btn0_during_long_press() {
-  if (millis() - long_press_start > btn0_long_press_timeout) {
-    digitalWrite(LED_BUILTIN, HIGH);
-
-  }
 }
 
 void btn0_long_press_release() {
   long_press_start = 0;
-  during_long_press = false;
 }
 
 void print_val() {
-  Serial.printf("%lu\t%.1f\t\t%.1f\t%.1f\t%+.1f\n",
-              millis()/1000,
+  Datime dt = NTP;
+
+  Serial.printf("%s\t%.1f\t\t%.1f\t%.1f\t%+.1f\n",
+              dt.toString().c_str(),
               mic_signal.avg_dB,
               mic_signal.avg_dB,
               mic_signal.rms_voltage*1000,  // RMS в mV
               (mic_signal.rms_voltage * 1000) - 12.5);  // Отклонение от 12.5mV
 
   led_flag = !led_flag;
-  if (!during_long_press)  digitalWrite(LED_BUILTIN, led_flag);
+  if (!time_error)  digitalWrite(LED_BUILTIN, led_flag);
 }
 
 Ticker print_ticker(print_val, 1000);
@@ -104,18 +98,7 @@ void setup() {
   Serial.begin(115200);
   delay(1000);
 
-  // reset settings - wipe stored credentials for testing
-  // these are stored by the esp library
-  // wm.resetSettings();
-
-  // Automatically connect using saved credentials,
-  // if connection fails, it starts an access point with the specified name ( "AutoConnectAP"),
-  // if empty will auto generate SSID, if password is blank it will be anonymous AP (wm.autoConnect())
-  // then goes into a blocking loop awaiting configuration and will return success result
-
   bool res = false;
-  // res = wm.autoConnect(); // auto generated AP name from chipid
-  // res = wm.autoConnect("AutoConnectAP"); // anonymous ap
   res = wm.autoConnect("Mic_Relay"); // password protected ap
 
   if(!res) {
@@ -125,9 +108,10 @@ void setup() {
   else {
     //if you get here you have connected to the WiFi
     Serial.println("connected...yeey :)");
+
+    WiFi.setAutoReconnect(true); // Ensure auto reconnect is enabled
+    WiFi.persistent(true); // Ensure credentials are saved persistently
   }
-
-
 
   Serial.println("\n=== Измерение громкости с MAX9814 (смещение 1.25V) ===");
 
@@ -151,7 +135,7 @@ void setup() {
   digitalWrite(RELAY1, LOW);
   digitalWrite(RELAY2, LOW);
 
-  onboard_btn.setPressMs(500);
+  onboard_btn.setPressMs(1000);
   onboard_btn.attachClick(btn0_click);
   onboard_btn.attachLongPressStart(btn0_pressed);
 
@@ -186,7 +170,7 @@ void setup() {
   // });
 
   NTP.begin(3);                           // запустить и указать часовой пояс
-  // NTP.setPeriod(30);                   // период синхронизации в секундах
+  NTP.setPeriod(60);                   // период синхронизации в секундах
   // NTP.setHost("ntp1.stratum2.ru");     // установить другой хост
   // NTP.setHost(IPAddress(1, 2, 3, 4));  // установить другой хост
   NTP.asyncMode(false);                // выключить асинхронный режим
@@ -215,21 +199,15 @@ void loop() {
   onboard_btn.tick();
   print_ticker.update();
 
-
-  // if (NTP.tick()) {
-  //   // вывод даты и времени строкой
-  //   Serial.print(NTP.toString());  // NTP.timeToString(), NTP.dateToString()
-  //   Serial.print(':');
-  //   Serial.println(NTP.ms());  // + миллисекунды текущей секунды. Внутри tick всегда равно 0
-  // }
-
-  // if (NTP.newSecond()) {
-  //   // новую секунду можно поймать и здесь
-  // }
+  NTP.tick();
 
   // изменился онлайн-статус
   if (NTP.statusChanged()) {
     Serial.print("STATUS: ");
     Serial.println(NTP.online());
+    if (!NTP.online() || NTP.hasError()) time_error = true;
+    else time_error = false;
   }
+
+  if (time_error) digitalWrite(LED_BUILTIN, true);
 }
